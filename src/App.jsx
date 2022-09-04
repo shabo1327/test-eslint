33d393fd28f061b45fc139ce4ec52f9698aa72a7
shabo1327 <aldair1327@gmail.com>
2022-09-04 11:02:35 -0500

    tersting this thing again...

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,7 @@ function App() {
 
   const [count, setCount] = useState(0)
 
-  return
-  (
+  return (
     <div className="App">
       <div>
         <a href="https://vitejs.dev" target="_blank">
