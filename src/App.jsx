7e7efb00a65ba8b061b92dac7a9984efa4e81c9f
shabo1327 <aldair1327@gmail.com>
2022-09-04 09:59:11 -0500

    eslint test

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 
+
+
 function App() {
+  const x = 0;
   const [count, setCount] = useState(0)
 
   return (
